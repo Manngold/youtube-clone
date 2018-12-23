9211060994393b11751abd6a5a60603260f69869
Manngold <jsw4820@gmail.com>
2018-12-24 00:30:05 +0900

    dev setting

---
+++ index.js
@@ -0,0 +1,16 @@
+import express from "express";
+const app = express();
+
+const PORT = 4000;
+
+const handleListening = () =>
+  console.log(`Listening on: http://localhost:${PORT}`);
+
+const handleProfile = (req, res) => res.send("Here is profile page");
+
+const handleHome = (req, res) => res.send("Hello from home");
+
+app.get("/profile", handleProfile);
+app.get("/", handleHome);
+
+app.listen(PORT, handleListening);
