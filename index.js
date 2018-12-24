ca891ef732dec9467a0790d6f8ebb4b2fe418cb7
Manngold <jsw4820@gmail.com>
2018-12-24 22:03:32 +0900

    what is middlewares

--- index.js
+++ index.js
@@ -10,7 +10,14 @@ const handleProfile = (req, res) => res.send("Here is profile page");
 
 const handleHome = (req, res) => res.send("Hello from home");
 
-app.get("/profile", handleProfile);
+const betweenHome = (req, res, next) => {
+  console.log("i'm between");
+  next();
+};
+
+app.use(betweenHome); //global middleware
+
 app.get("/", handleHome);
+app.get("/profile", handleProfile);
 
 app.listen(PORT, handleListening);
