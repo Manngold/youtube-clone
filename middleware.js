5b72c686c2a01bd58715c9b2a7cc908734c6ebbf
Manngold <jsw4820@gmail.com>
2018-12-27 14:26:59 +0900

    add views

--- middleware.js
+++ middleware.js
@@ -4,7 +4,5 @@ export const localMiddleware = (req, res, next) => {
   res.locals.siteName = "Youtube";
   res.locals.developer = "Manngold";
   res.locals.routes = routes;
-  res.locals.currentPath = req.path;
-
   next();
 };
