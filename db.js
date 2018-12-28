0c2fc184ada4168067e650cda4f80895cad50037
Manngold <jsw4820@gmail.com>
2018-12-28 11:28:03 +0900

    add join controller

---
+++ db.js
@@ -0,0 +1,46 @@
+export const videos = [
+  {
+    id: 3211423,
+    title: "Whiplash",
+    description: "This is something I love",
+    views: 24,
+    videoFile: "https://www.youtube.com/embed/tgbNymZ7vqY?autoplay=1",
+    creator: {
+      name: "Manngold",
+      email: "jsw4820@naver.com"
+    }
+  },
+  {
+    id: 32114,
+    title: "Whiplash",
+    description: "This is something I love",
+    views: 24,
+    videoFile: "https://www.youtube.com/embed/tgbNymZ7vqY?autoplay=1",
+    creator: {
+      name: "Manngold",
+      email: "jsw4820@naver.com"
+    }
+  },
+  {
+    id: 321,
+    title: "Whiplash",
+    description: "This is something I love",
+    views: 24,
+    videoFile: "https://www.youtube.com/embed/tgbNymZ7vqY?autoplay=1",
+    creator: {
+      name: "Manngold",
+      email: "jsw4820@naver.com"
+    }
+  },
+  {
+    id: 32,
+    title: "Whiplash",
+    description: "This is something I love",
+    views: 24,
+    videoFile: "https://www.youtube.com/embed/tgbNymZ7vqY?autoplay=1",
+    creator: {
+      name: "Manngold",
+      email: "jsw4820@naver.com"
+    }
+  }
+];
