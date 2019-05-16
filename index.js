1136ab8ee761f4ade24f435eb014438cb9834dcb
manngold <jsw4820@gmail.com>
2019-05-16 15:44:15 +0900

    handle routes using express

---
+++ index.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const app = express();
+const PORT = 4000;
+
+const handleListening = () => {
+  console.log(`Server is online on http://localhost:${PORT}`);
+};
+
+const handleHome = (req, res) => {
+  res.send("hi there");
+};
+
+const handleProfile = (req, res) => {
+  res.send("here is profile");
+};
+
+app.get("/", handleHome);
+app.get("/profile", handleProfile);
+
+app.listen(PORT, handleListening);
