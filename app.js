04373f1795e65542b477c56d6917a069a4563e2e
manngold <jsw4820@gmail.com>
2019-05-17 22:07:07 +0900

    divide router by MVC pattern

--- app.js
+++ app.js
@@ -3,17 +3,11 @@ import morgan from "morgan";
 import helmet from "helmet";
 import cookieParser from "cookie-parser";
 import bodyParser from "body-parser";
-import { userRouter } from "./router";
+import userRouter from "./routers/userRouter";
+import videoRouter from "./routers/videoRouter";
+import globalRouter from "./routers/globalRouter";
+
 const app = express();
-
-const handleHome = (req, res) => {
-  res.send("hi there");
-  console.log("final result");
-};
-
-const handleProfile = (req, res) => {
-  res.send("here is profile");
-};
 
 app.use(cookieParser());
 app.use(bodyParser.json());
@@ -21,8 +15,8 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(helmet());
 app.use(morgan("dev"));
 
+app.use("/", globalRouter);
 app.use("/user", userRouter);
-app.get("/", handleHome);
-app.get("/profile", handleProfile);
+app.use("/video", videoRouter);
 
 export default app;
