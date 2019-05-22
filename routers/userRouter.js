9169cbed8445b9e52b98d8dabf2164a893b9f239
장서원 <manngold@jangseowon-ui-MacBook-Pro.local>
2019-05-21 23:52:51 -0700

    routes setting

--- routers/userRouter.js
+++ routers/userRouter.js
@@ -1,5 +1,11 @@
 import express from "express";
+import routes from "../routes";
 
 const userRouter = express.Router();
+
+userRouter.get(routes.users, (req, res) => res.send("users page"));
+userRouter.get(routes.userDetail, (req, res) => res.send("userdetail page"));
+userRouter.get(routes.editProfile, (req, res) => res.send("edit page"));
+userRouter.get(routes.changePassword, (req, res) => res.send("change password page"));
 
 export default userRouter;
