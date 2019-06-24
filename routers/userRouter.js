b5ed57db5641b7b4ab045e652c629a727561d8e2
Manngold <jsw4820@gmail.com>
2019-06-24 15:41:36 +0900

    add view engine and test

--- routers/userRouter.js
+++ routers/userRouter.js
@@ -7,6 +7,7 @@ const userRouter = express.Router();
 userRouter.get(routes.users, users);
 userRouter.get(routes.userDetail, userDetail);
 userRouter.get(routes.editProfile, editProfile);
-userRouter.get(routes.changePassword, changePassword);
+userRouter.get(routes.changePassword, changePassword); 
+
 
 export default userRouter;
