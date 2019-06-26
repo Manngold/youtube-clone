fb89f90694e13fabb2a8ca6ac2cceaa833d922ed
Manngold <jsw4820@gmail.com>
2019-06-26 18:00:38 +0900

    send js info to template

--- app.js
+++ app.js
@@ -3,19 +3,22 @@ import morgan from "morgan";
 import helmet from "helmet";
 import cookieParser from "cookie-parser";
 import bodyParser from "body-parser";
+import routes from "./routes";
 import userRouter from "./routers/userRouter";
 import videoRouter from "./routers/videoRouter";
 import globalRouter from "./routers/globalRouter";
-import routes from "./routes";
+import { localMiddleware } from "./middlewares";
 
 const app = express();
 
+app.use(helmet());
 app.use(cookieParser());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(helmet());
 app.use(morgan("dev"));
 app.set("view engine", "pug");
+
+app.use(localMiddleware);
 
 app.use(routes.home, globalRouter);
 app.use(routes.users, userRouter);
--- controllers/videoController.js
+++ controllers/videoController.js
@@ -1,7 +1,7 @@
-export const home = (req, res) => res.render("home", { title : "home"});
-export const search = (req, res) => res.send("search");
-export const videos = (req, res) => res.send("videos");
-export const upload = (req, res) => res.send("upload");
-export const videoDetail = (req, res) => res.send("videoDetail");
-export const editVideo = (req, res) => res.send("editVideo");
-export const deleteVideo = (req, res) => res.send("deleteVideo");
+export const home = (req, res) => res.render("home", { pageTitle : "Home"});
+export const search = (req, res) => res.send("search", { pageTitle : "Search"});
+export const videos = (req, res) => res.send("videos", { pageTitle : "Videos"});
+export const upload = (req, res) => res.send("upload", { pageTitle : "Upload"});
+export const videoDetail = (req, res) => res.send("videoDetail", { pageTitle : "Video Detail"});
+export const editVideo = (req, res) => res.send("editVideo", { pageTitle : "Edit Video"});
+export const deleteVideo = (req, res) => res.send("deleteVideo", { pageTitle : "Delete Video"});
