7105722eceaeb65c736f40965e3a3c12d35aa643
Manngold <jsw4820@gmail.com>
2019-06-27 23:31:13 +0900

    fix controller by pug

--- controllers/videoController.js
+++ controllers/videoController.js
@@ -1,7 +1,10 @@
 export const home = (req, res) => res.render("home", { pageTitle : "Home"});
-export const search = (req, res) => res.send("search", { pageTitle : "Search"});
-export const videos = (req, res) => res.send("videos", { pageTitle : "Videos"});
-export const upload = (req, res) => res.send("upload", { pageTitle : "Upload"});
-export const videoDetail = (req, res) => res.send("videoDetail", { pageTitle : "Video Detail"});
-export const editVideo = (req, res) => res.send("editVideo", { pageTitle : "Edit Video"});
-export const deleteVideo = (req, res) => res.send("deleteVideo", { pageTitle : "Delete Video"});
+export const search = (req, res) => {
+    console.log(req.query);
+    res.render("search", { pageTitle : "Search"});
+};
+export const videos = (req, res) => res.render("videos", { pageTitle : "Videos"});
+export const upload = (req, res) => res.render("upload", { pageTitle : "Upload"});
+export const videoDetail = (req, res) => res.render("videoDetail", { pageTitle : "Video Detail"});
+export const editVideo = (req, res) => res.render("editVideo", { pageTitle : "Edit Video"});
+export const deleteVideo = (req, res) => res.render("deleteVideo", { pageTitle : "Delete Video"});
