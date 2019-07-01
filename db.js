f97fc7301b07dd2ded38dcd15b1de1cfaed16c66
Manngold <jsw4820@gmail.com>
2019-07-01 16:52:16 +0900

    home controller

---
+++ db.js
@@ -0,0 +1,38 @@
+export const videos = [
+    {
+        id: 31244,
+        title: 'video test',
+        description: 'this is something i love',
+        views: 24,
+        videoFile: "http://media.w3.org/2010/05/sintel/trailer.mp4",
+        creator:{
+            id: 1231323,
+            name: "manngold",
+            email: "jsw4820@naver.com"
+        }
+    },
+    {
+        id: 123414,
+        title: 'no way',
+        description: 'this is something i love',
+        views: 24,
+        videoFile: "http://media.w3.org/2010/05/sintel/trailer.mp4",
+        creator:{
+            id: 1231323,
+            name: "manngold",
+            email: "jsw4820@naver.com"
+        }
+    },
+    {
+        id: 11122,
+        title: 'video test2',
+        description: 'this is something i love',
+        views: 24,
+        videoFile: "http://media.w3.org/2010/05/sintel/trailer.mp4",
+        creator:{
+            id: 1231323,
+            name: "manngold",
+            email: "jsw4820@naver.com"
+        }
+    }
+]
