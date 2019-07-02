6c4180754a7f736db08853187b0836372e444766
Manngold <jsw4820@gmail.com>
2019-07-02 16:39:35 +0900

    connecting to MongoDB

--- db.js
+++ db.js
@@ -1,38 +1,16 @@
-export const videos = [
-    {
-        id: 31244,
-        title: 'video test',
-        description: 'this is something i love',
-        views: 24,
-        videoFile: "http://media.w3.org/2010/05/sintel/trailer.mp4",
-        creator:{
-            id: 1231323,
-            name: "manngold",
-            email: "jsw4820@naver.com"
-        }
-    },
-    {
-        id: 123414,
-        title: 'no way',
-        description: 'this is something i love',
-        views: 24,
-        videoFile: "http://media.w3.org/2010/05/sintel/trailer.mp4",
-        creator:{
-            id: 1231323,
-            name: "manngold",
-            email: "jsw4820@naver.com"
-        }
-    },
-    {
-        id: 11122,
-        title: 'video test2',
-        description: 'this is something i love',
-        views: 24,
-        videoFile: "http://media.w3.org/2010/05/sintel/trailer.mp4",
-        creator:{
-            id: 1231323,
-            name: "manngold",
-            email: "jsw4820@naver.com"
-        }
-    }
-]
+import mongoose from "mongoose";
+
+
+mongoose.connect("mongodb://localhost:27017/wetube", {
+    useNewUrlParser: true,
+    useFindAndModify: false
+});
+
+const db = mongoose.connection;
+
+const handleOpen = () => console.log("Database is online 💽");
+
+const handleError = error => console.log(`Error on Database Connection : ${error}`);
+
+db.once("open", handleOpen);
+db.on("error", handleError);
