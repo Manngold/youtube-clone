6feb0982774436c5f6525710d52e48ef7648133b
Manngold <jsw4820@gmail.com>
2019-07-21 19:06:52 +0900

    delete video using mongoose

--- controllers/videoController.js
+++ controllers/videoController.js
@@ -3,7 +3,7 @@ import Video from "../models/Video";
 
 export const home = async (req, res) => {
     try {
-        const videos = await Video.find({});
+        const videos = await Video.find({}).sort({ _id: -1 });
         res.render("home", { pageTitle: "Home", videos });
     } catch (error) {
         console.log(error);
@@ -41,7 +41,7 @@ export const videoDetail = async (req, res) => {
             params: { id }
         } = req;
         const video = await Video.findById(id);
-        res.render("videoDetail", { pageTitle: "Video Detail", video });
+        res.render("videoDetail", { pageTitle: video.title, video });
     } catch (error) {
         console.log(error);
         res.redirect(routes.home);
@@ -76,5 +76,14 @@ export const postEditVideo = async (req, res) => {
     }
 };
 
-export const deleteVideo = (req, res) =>
-    res.render("deleteVideo", { pageTitle: "Delete Video" });
+export const deleteVideo = async (req, res) => {
+    const {
+        params: { id }
+    } = req;
+    try {
+        await Video.findOneAndDelete({ _id: id });
+    } catch (error) {
+        console.log(error);
+    }
+    res.redirect(routes.home);
+};
